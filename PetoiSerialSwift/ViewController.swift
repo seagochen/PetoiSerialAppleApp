2dcf1357badeda01f7e915eeacdf5f354246296f
Orlando Chen <seagochen@hotmail.com>
2021-03-23 12:29:01 +0800

    Initial Commit

---
+++ PetoiSerialSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetoiSerialSwift
+//
+//  Created by Orlando Chen on 2021/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
